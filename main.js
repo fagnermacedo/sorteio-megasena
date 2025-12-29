a3f7ffc9d06f2ebc0d182e424ac1e29eefec241f
Fagner Sá <fagnermacedo@yahoo.com.br>
2025-12-29 13:53:26 -0300

    Correção código. Não permitir números repetidos
    Não permitir números repetidos no mesmo jogo e permitir jogos entre 6 e 15 números.

--- main.js
+++ main.js
@@ -1,69 +1,69 @@
 // Calcular custo Bolão **********************************************
-
 function calcularCusto(numNumeros) {
-    const tabelaPrecos = {
-      6: 6,
-      7: 42,
-      8: 168.0,
-      9: 504.0,
-      10: 1260.0,
-      11: 2772.0,
-      12: 5544.0,
-      13: 10296.0,
-      14: 18018.0,
-      15: 30030.0,
-    };
-    return tabelaPrecos[numNumeros] || 0;
+  const tabelaPrecos = {
+    6: 6,
+    7: 42,
+    8: 168.0,
+    9: 504.0,
+    10: 1260.0,
+    11: 2772.0,
+    12: 5544.0,
+    13: 10296.0,
+    14: 18018.0,
+    15: 30030.0,
+  };
+  return tabelaPrecos[numNumeros] || 0;
+}
+
+function calcularCustoBolao() {
+  let numApostas = parseInt(document.getElementById("numApostas").value);
+  let numNumeros = parseInt(document.getElementById("numNumeros").value);
+  let numCotas = parseInt(document.getElementById("numCotas").value);
+
+  if (
+    isNaN(numApostas) ||
+    isNaN(numNumeros) ||
+    numNumeros < 6 ||
+    numNumeros > 15
+  ) {
+    alert(
+      "Por favor, insira valores válidos. O número de números por aposta deve estar entre 6 e 15."
+    );
+    return;
   }
-  function calcularCustoBolao() {
-    let numApostas = parseInt(document.getElementById("numApostas").value);
-    let numNumeros = parseInt(document.getElementById("numNumeros").value);
-    let numCotas = parseInt(document.getElementById("numCotas").value);
-    if (
-      isNaN(numApostas) ||
-      isNaN(numNumeros) ||
-      numNumeros < 6 ||
-      numNumeros > 15
-    ) {
-      alert(
-        "Por favor, insira valores válidos. O número de números por aposta deve estar entre 6 e 15."
-      );
-      return;
-    }
-    let custoPorAposta = calcularCusto(numNumeros);
-    let custoTotal = numApostas * custoPorAposta;
-    let custoPorCota = custoTotal /numCotas;
-    document.getElementById("resultado").value = "Custo Total do Bolão: R$ " + custoTotal.toFixed(2) + "\n";
-    document.getElementById("resultado").value += "Custo por Cota no Bolão: R$ " + custoPorCota.toFixed(2);
-  }
-  
+
+  let custoPorAposta = calcularCusto(numNumeros);
+  let custoTotal = numApostas * custoPorAposta;
+  let custoPorCota = custoTotal / numCotas;
+
+  document.getElementById("resultado").value =
+    "Custo Total do Bolão: R$ " + custoTotal.toFixed(2) + "\n";
+  document.getElementById("resultado").value +=
+    "Custo por Cota no Bolão: R$ " + custoPorCota.toFixed(2);
+}
 
 //##############################################################################################
 
 function sortearNumero(min, max) {
-  // Gera um número aleatório entre min e max
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 
 let contadorJogo = 1;
 
 function sortearNumeros() {
-  // Obtém os valores dos campos de entrada
   let numeroInicial = parseInt(document.getElementById("numeroInicial").value);
   let numeroFinal = parseInt(document.getElementById("numeroFinal").value);
   let numeroTotaldeJogos = parseInt(
     document.getElementById("numeroTotalJogo").value
   );
 
-  // Verifica se os valores são válidos
   if (
     isNaN(numeroInicial) ||
     isNaN(numeroFinal) ||
     isNaN(numeroTotaldeJogos) ||
     numeroInicial > numeroFinal ||
-    numeroTotaldeJogos <= 6 ||
-    numeroTotaldeJogos >= 15
+    numeroTotaldeJogos < 6 ||
+    numeroTotaldeJogos > 15
   ) {
     alert(
       "Por favor, insira valores válidos e certifique-se de que o número inicial é menor ou igual ao número final."
@@ -72,14 +72,25 @@ function sortearNumeros() {
   }
 
   let numerosSorteados = [];
-//   console.log(numeroTotaldeJogos); //Teste
-  for (let i = 0; i < numeroTotaldeJogos; i++) {
-    numerosSorteados.push(sortearNumero(numeroInicial, numeroFinal));
+
+  // Sorteio sem repetição
+  while (numerosSorteados.length < numeroTotaldeJogos) {
+    let numero = sortearNumero(numeroInicial, numeroFinal);
+
+    if (!numerosSorteados.includes(numero)) {
+      numerosSorteados.push(numero);
+    }
   }
 
-  // Adiciona os números sorteados ao conteúdo existente do textarea
+  // Ordena os números
+  numerosSorteados.sort((a, b) => a - b);
+
+  // Exibe no textarea
   let resultadoTextarea = document.getElementById("resultado1");
-//   console.log("Saida text area ",+ resultadoTextarea); //Teste
-  resultadoTextarea.value += (`Jogo: ${contadorJogo}º:   `) +numerosSorteados.join(", ") + "\n";
+  resultadoTextarea.value +=
+    `Jogo: ${contadorJogo}º:   ` +
+    numerosSorteados.join(", ") +
+    "\n";
+
   contadorJogo++;
 }
